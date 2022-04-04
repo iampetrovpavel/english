c80bd12ef1de0d276fb409e18f1e16417ec9f1d4
Pavel <iampetrovpavel@gmail.com>
2022-04-04 10:01:30 +0300

    fix bug with ios overflow

--- client/src/components/errors.jsx
+++ client/src/components/errors.jsx
@@ -6,10 +6,20 @@ import useModal from 'antd/lib/modal/useModal';
 const Errors = ({errors}) => {
     useEffect(()=>{
         if(!errors)return
-        Modal.error({
-          title: `Ошибка ${ errors.code }`,
-          content: errors.data.map(e=><Space key={e.message} direction="vertical">{e.message}</Space>)
-        })
+        //CUSTOM MOLECULER ERROR
+        if(errors.message){
+          Modal.error({
+            title: `Ошибка ${ errors.code }`,
+            content: errors.message
+          })
+        }
+        //VALIDATION ERRORS
+        if(errors.data && Array.isArray(errors.data)){
+          Modal.error({
+            title: `Ошибка ${ errors.code }`,
+            content: errors.data.map((e, i)=><Space key={e.message || i} direction="vertical">{e.message || 'Неизвестная ошибка...'}</Space>)
+          })
+        }
       }, [errors])
       return null
 }
--- client/src/pages/Play.jsx
+++ client/src/pages/Play.jsx
@@ -7,7 +7,7 @@ const Play = () => {
     const { list, fetch } = useRandomList()
     const [ remain, setRemain ] = useState(0)
     const [ answer, setAnswer ] = useState('')
-    const [ next, showNext ] = useState(false)
+    const [ nextButton, showNextButton ] = useState(false)
     const { isModalVisible, showModal, handleOk, handleCancel } = useModal()
     const [ stat, setStat ] = useState([])
     const cardRef = useRef(null)
@@ -24,7 +24,7 @@ const Play = () => {
     function handleForgot() {
         inputRef.current.input.classList.add('fail')
         setAnswer(list[remain-1].trans[0])
-        showNext(true)
+        showNextButton(true)
         setStat([...stat, {word: list[remain-1], success: false}])
     }
 
@@ -33,7 +33,7 @@ const Play = () => {
         setTimeout(()=>{
             inputRef.current.input.classList.remove('fail')
             showNewCard()
-            showNext(false)
+            showNextButton(false)
         }, 300)
     }
 
@@ -56,7 +56,7 @@ const Play = () => {
     }
 
     function handleAnswerInput(e) {
-        if(next)return;
+        if(nextButton)return;
         const value = e.target.value;
         if( list[remain-1].trans.includes(value)){
             setStat([...stat, {word: list[remain-1], success: true}])
@@ -79,15 +79,15 @@ const Play = () => {
     }
     if(list.length === 0){return <h1>Loading...</h1>}
     return (
-        <div style={{paddingTop: '1em'}}>
+        <div style={{paddingTop: '1em', height: '100%'}}>
             {remain===0?<Button type="link" success="true" onClick={ startNewGame }>
                             Начать тренировку
                         </Button>
                         :<h3>Осталось { remain } слов{inclination(remain)}</h3>}
-            <Card ref={cardRef} size="small" title={ list[remain?remain-1:0].word } style={{ width: 300, marginTop: '1em' }} className='animate__animated'>
+            <Card ref={cardRef} size="small" title={ list[remain?remain-1:0].word } style={{ width: 300, marginTop: '1em', display:remain===0?'none':'block' }} className='animate__animated'>
                 <Space>
                     <Input ref={inputRef} placeholder="Слово на русском" value={ answer } onChange={ handleAnswerInput }/>
-                    {next?<Button type="link" success="true" onClick={ handleNext }>
+                    {nextButton?<Button type="link" success="true" onClick={ handleNext }>
                         Дальше
                     </Button>
                     :<Button type="link" success="true" onClick={ handleForgot }>
@@ -108,13 +108,5 @@ const Play = () => {
         </div>
     )
 }
-
-// const Word = ({ word }) => {
-//     return (
-//         <div>
-//             { word.word }
-//         </div>
-//     )
-// }
 
 export default Play
